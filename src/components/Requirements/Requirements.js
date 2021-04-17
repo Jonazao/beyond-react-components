c25721ff19727b4624d6914993cd2bfc78655ea1
Jhonnatan <JhonnatanGuerreroM@gmail.com>
2021-04-17 13:39:58 -0700

    Add MaterialUi

--- src/components/Requirements/Requirements.js
+++ src/components/Requirements/Requirements.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import { Button } from '@material-ui/core';
 export const Requirements = () => {
-  return <>Hello World</>;
+  return <Button variant="contained" color="primary">Hello World</Button>;
 };
